7ba191f80e777bf5381a228d7a6d5a38be8adec8
Natalia Maximo <nmaximo@microsoft.com>
2023-03-08 09:34:37 -0500

    clippy lints on healthcheck

--- healthcheck/src/main.rs
+++ healthcheck/src/main.rs
@@ -1,10 +1,10 @@
 use std::{env, process::ExitCode};
 
 fn main() -> ExitCode {
-    let port = env::var("ROCKET_PORT").unwrap_or(String::from("8000"));
+    let port = env::var("ROCKET_PORT").unwrap_or_else(|_| String::from("8000"));
     let endpoint = format!("http://localhost:{}/heartbeat", port);
 
-    let res = minreq::get(&endpoint).send();
+    let res = minreq::get(endpoint).send();
 
     if res.is_err() {
         println!("{}", res.unwrap_err());
@@ -13,7 +13,7 @@ fn main() -> ExitCode {
 
     let code = res.unwrap().status_code;
 
-    if code < 200 || code > 299 {
+    if !(200..=299).contains(&code) {
         println!("Received status code {}", code);
         return ExitCode::from(1);
     }
